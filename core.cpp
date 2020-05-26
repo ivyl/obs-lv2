a09bb04c32f0390aeb3d4d5392d40a1eed527d9d
Arkadiusz Hiler <arek@hiler.eu>
2020-05-26 19:23:49 +0300

    Add support for spectral plugins

--- core.cpp
+++ core.cpp
@@ -91,6 +91,7 @@ void LV2Plugin::populate_supported_plugins(void)
 	supported_classes.push_back(lilv_new_uri(this->world, LV2_CORE__EQPlugin));
 	supported_classes.push_back(lilv_new_uri(this->world, LV2_CORE__ModulatorPlugin));
 	supported_classes.push_back(lilv_new_uri(this->world, LV2_CORE__SpatialPlugin));
+	supported_classes.push_back(lilv_new_uri(this->world, LV2_CORE__SpectralPlugin));
 
 	LILV_FOREACH(plugins, i, this->plugins) {
 		auto plugin = lilv_plugins_get(this->plugins, i);
